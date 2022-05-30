325d9a7f3e0fb7ead4450c6e88869f0e23b9383c
ktman-ktman <ktym.disk.mobile.note@gmail.com>
2022-05-30 12:23:44 +0000

    環境設定

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+initial_dir=${PWD}
+
+# dotfiles
+git clone https://github.com/ktman-ktman/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+./install_vscode.sh
+cd ${initial_dir}
+
+# poetry
+if [[ ! -f "./.venv" ]] && [[ -f "./poetry.lock" ]]
+then
+	poetry install
+fi
